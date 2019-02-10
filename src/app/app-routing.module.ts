2eb59ee4e2e9ea585e9341c87fc5a7ee61e50c83
RajARROW <30944041+RajARROW@users.noreply.github.com>
2019-02-10 21:29:54 +0530

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventComponent } from './event/event.component';
+import { IntroComponent } from './intro/intro.component';
+import { TeamComponent } from './team/team.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FAQComponent } from './faq/faq.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'event', component: EventComponent },
+  { path: '', component: IntroComponent },
+  { path: 'aboutus', component: AboutUsComponent },
+  { path: 'contactus', component: ContactUsComponent },
+  { path: 'faq', component: FAQComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'team', component: TeamComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
