64af67d4e48b863ad41437c002691a7830a13c1f
RajARROW <30944041+RajARROW@users.noreply.github.com>
2019-02-12 15:05:31 +0530

    Dynamic

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventComponent implements OnInit {
 
+
+  v: any;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.v = [
+      {Event_Name: 'Event Name 1',
+        dec: 'Quam adipiscing vitae proin Nec feugiat nisl pretium',
+        ID: '1'
+      },
+      {Event_Name: 'Event Name 1',
+        dec: 'Quam adipiscing vitae proin Nec feugiat nisl pretium',
+        ID: '2'
+      },
+      {Event_Name: 'Event Name 1',
+        dec: 'Quam adipiscing vitae proin Nec feugiat nisl pretium',
+        ID: '3'
+      }
+    ];
+
+  }
+
+  public register(ID){
+
+    console.log(ID);
   }
 
 }
