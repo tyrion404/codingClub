bfd3448cf58886f8f641c00dcc4e8963b7554660
dk445 <kartik.dambre@gmail.com>
2019-02-21 20:42:58 +0530

    Gitacora saved 1 changed files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyB30s0J6YXG2vjrpXb2ebxZs07MvMcBwQk",
+    authDomain: "codingclub-f3cba.firebaseapp.com",
+    databaseURL: "https://codingclub-f3cba.firebaseio.com",
+    projectId: "codingclub-f3cba",
+    storageBucket: "codingclub-f3cba.appspot.com",
+    messagingSenderId: "161009164795"
+  }
 };
 
 /*
