90e4e57279688b20623836580f791d247a7a9fa3
dk445 <kartik.dambre@gmail.com>
2019-02-26 21:43:19 +0530

    Gitacora saved 3 changed files

--- src/app/service/infoSub.ts
+++ src/app/service/infoSub.ts
@@ -1,15 +1,15 @@
 export interface SubIndi {
-        $pEmail: string;
+        pEmail: string;
         pName: string;
         pPhone: number;
 }
 export interface SubTeam {
-        $lEmail: string;
+        lEmail: string;
         lName: string;
         lPhone: number;
 }
 export interface Team {
-        $mEmail: string;
+        mEmail: string;
         mName: string;
         mPhone: number;
 }
