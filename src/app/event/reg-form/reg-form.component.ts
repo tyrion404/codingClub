76c9869e2621c8b4eb7060836210c6832dc6356b
dk445 <kartik.dambre@gmail.com>
2019-03-05 02:04:49 +0530

    Gitacora saved 3 changed files

--- src/app/event/reg-form/reg-form.component.ts
+++ src/app/event/reg-form/reg-form.component.ts
@@ -1,25 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 //'../../../../node_modules/@angular/core';
 //import {MODAL_BROWSER_PROVIDERS} from "angular2-modal/platform-browser/index";
-import {NgIf} from '../../../../node_modules/@angular/common';
+import { NgIf } from '../../../../node_modules/@angular/common';
 //import { ApicallService } from '../.././apicall.service';
 //import { checkBinding } from '@angular/core/src/view/util';
-import { SubIndi  } from '../../service/infoSub';
+import { SubIndi } from '../../service/infoSub';
 //import { CrudService } from 'src/app/service/crud.service';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable, of } from 'rxjs';
+import { EventComponent } from '../event.component';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { ApicallService } from 'src/app/apicall.service';
+import { switchMap } from 'rxjs/operators';
+import { User } from 'firebase';
+import * as firebase from 'firebase';
 //import { EventComponent } from '../event.component';
 
 //import { ApicallService } from '../../apicall.service';
+
+interface User {
+  uid: string;
+  email: string;
+}
+
 @Component({
   selector: 'app-reg-form',
   templateUrl: './reg-form.component.html',
   styleUrls: ['./reg-form.component.css']
 })
-export class RegFormComponent implements OnInit{
+export class RegFormComponent implements OnInit {
 
   db: AngularFirestore;
   teamMem: number;
+  user: Observable<User>;
   ID: any;
   pEmail: string;
   pName: string;
@@ -27,59 +41,95 @@ export class RegFormComponent implements OnInit{
   items: Observable<any[]>;
 
   ngOnInit() {
-   
+    this.signIn();
   }
 
-  constructor(db: AngularFirestore) {
-    //super(ApicallService);
-    this.db = db;
-    this.items = this.db.collection('items').valueChanges();
-  }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router,
+    db: AngularFirestore,
+    private api: ApicallService){
+      this.db = db;
+      this.items = this.db.collection('items').valueChanges();
 
-  teamMembers(event: any){
+      //// Get auth data, then get firestore user document || null
+      this.user = this.afAuth.authState.pipe(
+        switchMap(user => {
+          if (user) {
+            return this.afs.doc<User>(`users/${user.uid}`).valueChanges()
+          } else {
+            return of(null)
+          }
+        })
+      )
+    }
+
+  teamMembers(event: any) {
     this.teamMem = event.target.value;
     console.log(event);
   }
 
-  infoSub01(){
+  infoSub01() {
     this.checkIndi();
     console.log();
   }
-
- /* infoSub02(){
-    this.checkTeam(this.getValue('choose'));
-    console.log();
-  }*/
-  checkIndi(){
+  checkIndi() {
     const name = this.getValue('pName');
     const email = this.getValue('pEmail');
     const phone = this.getValue('pPhone');
-   // const eventId = this.getValue('I.id');
-    if (name === '' || email === '' || phone === '') {alert('All fields are mandotory'); } else {
-        const obj = {
+    // const eventId = this.getValue('I.id');
+    if (name === '' || email === '' || phone === '') { alert('All fields are mandotory'); } else {
+      const obj = {
         name: this.getValue('pName'),
         email: this.getValue('pEmail'),
         phone: this.getValue('pPhone'),
         //eventId: this.eveId
       };
-        this.db.collection('items').add(obj);
-        console.log('passed');
-        //alert('submitted');
+      this.db.collection('items').add(obj);
+      console.log('passed');
+      //alert('submitted');
 
     }
 
   }
-
-  checkTeam(id){
-  }
-
-
   getValue(id) {
     return (document.getElementById(id) as HTMLInputElement).value;
-}
- 
+  }
+
   memNo() {
     const no = this.getValue('choose');
     console.log('member : ' + no);
   }
+
+  signIn() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    return this.oAuthLogin(provider);
+  }
+
+  private oAuthLogin(provider) {
+    return this.afAuth.auth.signInWithPopup(provider)
+      .then((credential) => {
+        this.updateUserData(credential.user)
+      })
+  }
+
+
+  private updateUserData(user) {
+    // Sets user data to firestore on login
+
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+
+    const data: User = {
+      uid: user.uid,
+      email: user.email,
+    }
+    console.log(data.email);
+    //this.pEmail = data.email;
+    (document.getElementById('pEmail') as HTMLInputElement).value=data.email;
+    return null;
+
+  }
+
+
 }
