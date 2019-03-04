dceeb73b0dca8df7f3e5a0f68563af80e28ed3fb
dk445 <kartik.dambre@gmail.com>
2019-03-04 23:36:49 +0530

    Gitacora saved 3 changed files

--- src/app/event/reg-form/reg-form.component.ts
+++ src/app/event/reg-form/reg-form.component.ts
@@ -1,41 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import {NgIf} from '@angular/common';
-import { ApicallService } from '../.././apicall.service';
-import { checkBinding } from '@angular/core/src/view/util';
+import { Component, OnInit } from '../../../../node_modules/@angular/core';
+import {NgIf} from '../../../../node_modules/@angular/common';
+//import { ApicallService } from '../.././apicall.service';
+//import { checkBinding } from '@angular/core/src/view/util';
+import { SubIndi  } from '../../service/infoSub';
+//import { CrudService } from 'src/app/service/crud.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+//import { EventComponent } from '../event.component';
 
+//import { ApicallService } from '../../apicall.service';
 @Component({
   selector: 'app-reg-form',
   templateUrl: './reg-form.component.html',
   styleUrls: ['./reg-form.component.css']
 })
-export class RegFormComponent implements OnInit {
+export class RegFormComponent implements OnInit{
 
+  db: AngularFirestore;
   teamMem: number;
   ID: any;
-  constructor(private api: ApicallService) { }
+  pEmail: string;
+  pName: string;
+  pPhone: number;
+  items: Observable<any[]>;
 
   ngOnInit() {
-    this.ID = this.api.getid();
-    console.log(this.api.getid());
+   
   }
 
-  teamMembers(event : any){
+  constructor(db: AngularFirestore) {
+    //super(ApicallService);
+    this.db = db;
+    this.items = this.db.collection('items').valueChanges();
+  }
+
+  teamMembers(event: any){
     this.teamMem = event.target.value;
     console.log(event);
   }
 
   infoSub01(){
-    const config = {
-      apiKey: "AIzaSyB30s0J6YXG2vjrpXb2ebxZs07MvMcBwQk",
-      authDomain: "codingclub-f3cba.firebaseapp.com",
-      databaseURL: "https://codingclub-f3cba.firebaseio.com",
-      projectId: "codingclub-f3cba",
-      storageBucket: "codingclub-f3cba.appspot.com",
-      messagingSenderId: "161009164795"
-    };
-   // firebase.initializeApp(config);
-    //this.checkIndi();
-    
+    this.checkIndi();
     console.log();
   }
 
@@ -43,24 +48,36 @@ export class RegFormComponent implements OnInit {
     this.checkTeam(this.getValue('choose'));
     console.log();
   }*/
- /* checkIndi(){
+  checkIndi(){
     const name = this.getValue('pName');
     const email = this.getValue('pEmail');
     const phone = this.getValue('pPhone');
-    if(name === '' || email === '' || phone === '') {alert('All fields are mandotory'); }
-
-    else{
+   // const eventId = this.getValue('I.id');
+    if (name === '' || email === '' || phone === '') {alert('All fields are mandotory'); } else {
+        const obj = {
+        name: this.getValue('pName'),
+        email: this.getValue('pEmail'),
+        phone: this.getValue('pPhone'),
+        //eventId: this.eveId
+      };
+        this.db.collection('items').add(obj);
+        console.log('passed');
+        //alert('submitted');
 
     }
 
   }
 
   checkTeam(id){
-    
   }
-*/
+
 
   getValue(id) {
     return (document.getElementById(id) as HTMLInputElement).value;
 }
+ 
+  memNo() {
+    const no = this.getValue('choose');
+    console.log('member : ' + no);
+  }
 }
