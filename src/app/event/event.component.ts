f0843cbb79a5384fd96e5136aeb69ea259cc0378
dk445 <kartik.dambre@gmail.com>
2019-04-23 00:34:08 +0530

    Update event.component.ts

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ApicallService } from '../apicall.service';
+// import { userInfo } from 'firebase/';
+
+
+
 
 @Component({
   selector: 'app-event',
@@ -10,24 +14,31 @@ export class EventComponent implements OnInit {
 
 
   v: any;
+  event: any;
+  email: string;
+  // user: User;
+  // public afAuth: AngularFireAuth;
+  constructor(private api: ApicallService) { }
 
-  constructor(private api:ApicallService) { }
+// constructor arg :  private api: ApicallService
 
   ngOnInit() {
-
     this.v = [
-      {Event_Name: 'Event Name 1',
+      {
+        Event_Name: 'Event Name 1',
         dec: 'Quam adipiscing vitae proin Nec feugiat nisl pretium',
         ID: '1',
         Url: `../../assets/img/blog/img1.jpg`
 
       },
-      {Event_Name: 'Event Name 2',
+      {
+        Event_Name: 'Event Name 2',
         dec: 'Quam adipiscing vitae proin Nec feugiat nisl pretium',
         ID: '2',
         Url: `../../assets/img/blog/img2.jpg`
       },
-      {Event_Name: 'Event Name 3',
+      {
+        Event_Name: 'Event Name 3',
         dec: 'Quam adipiscing vitae proin Nec feugiat nisl pretium',
         ID: '3',
         Url: `../../assets/img/blog/img3.jpg`
@@ -36,9 +47,16 @@ export class EventComponent implements OnInit {
 
   }
 
-  public register(ID){
-    console.log(ID);
-    this.api.setid(ID);
+  public register(ID) {
+     event = ID;
+     console.log('event : ' + ID);
+     this.api.setid(ID);
   }
 
 }
+/*export class EventID extends EventComponent {
+ // public  eventid = event;
+  public returnId(){
+    return event;
+  }
+}*/
