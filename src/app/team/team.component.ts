2941eed2ce44745a9dbf1a4f4b97b7a6b6c2595c
tyrion404 <d.mallory2756@student.sbccd.edu>
2019-07-12 17:58:23 +0530

    UI_new

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -14,55 +14,55 @@ export class TeamComponent implements OnInit {
   ngOnInit() {
 
     this.v = [
-      {mem_name: 'member 1',
-        mem_post: 'postPost',
+      {mem_name: 'Nikunj Prajapati',
+        mem_post: 'https://github.com/nikunjleo',
         ID: '1',
         Url: '../assets/img/team/1.jpg'
       },
-      {mem_name: 'member 2',
-        mem_post: 'postPost',
+      {mem_name: 'Bhargav Patel',
+        mem_post: 'https://github.com/bhargavpatel',
         ID: '2',
         Url: '../assets/img/team/2.jpg'
       },
-      {mem_name: 'member 3',
-        mem_post: 'postPost',
+      {mem_name: 'Raj Shailendra',
+        mem_post: 'https://github.com/Shailu10',
         ID: '3',
         Url: '../assets/img/team/3.jpg'
       },
-      {mem_name: 'member 4',
-        mem_post: 'postPost',
+      {mem_name: 'Yash Shah',
+        mem_post: 'https://github.com/yash539',
         ID: '4',
         Url: '../assets/img/team/4.jpg'
       },
-      {mem_name: 'member 5',
-        mem_post: 'postPost',
-        ID: '4',
-        Url: '../assets/img/team/4.jpg'
+      {mem_name: 'Snehil Patel',
+        mem_post: 'github.com/highman09',
+        ID: '5',
+        Url: '../assets/img/team/5.jpg'
       },
-      {mem_name: 'member 6',
-        mem_post: 'postPost',
-        ID: '4',
-        Url: '../assets/img/team/4.jpg'
+      {mem_name: 'Chintan Prajapati',
+        mem_post: 'https://github.com/tyrion404',
+        ID: '6',
+        Url: '../assets/img/team/6.jpg'
       },
       {mem_name: 'member 7',
         mem_post: 'postPost',
         ID: '4',
-        Url: '../assets/img/team/4.jpg'
+        Url: '../assets/img/team/7.jpg'
       },
       {mem_name: 'member 8',
         mem_post: 'postPost',
         ID: '4',
-        Url: '../assets/img/team/4.jpg'
+        Url: '../assets/img/team/8.jpg'
       },
       {mem_name: 'member 9',
         mem_post: 'postPost',
         ID: '4',
-        Url: '../assets/img/team/4.jpg'
+        Url: '../assets/img/team/9.jpg'
       },
       {mem_name: 'member 10',
         mem_post: 'postPost',
         ID: '4',
-        Url: '../assets/img/team/4.jpg'
+        Url: '../assets/img/team/10.jpg'
       }
     ];
 
